b927b226b2275a328aba5a188d4e64a86675d9d2
nougght <muhamedmakusev@gmail.com>
2026-05-16 01:44:36 +0300

    refactor: server shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -3,21 +3,20 @@ package main
 import (
 	"context"
 	"log"
-	"net"
 	"net/http"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"simple-server/internal/handler"
 	"simple-server/internal/model"
 	"simple-server/internal/service"
-	"simple-server/internal/handler"
 )
 
 func main() {
 	config, err := model.LoadConfig()
 	if err != nil {
-		log.Println("ошибка при загрузке конфигурации")
+		log.Println("Ошибка при загрузке конфигурации")
 		panic(err)
 	}
 
@@ -28,39 +27,31 @@ func main() {
 	rootCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	serverCtx, stopServer := context.WithCancel(context.Background())
 	server := &http.Server{
-		Addr: ":8085",
-		BaseContext: func(_ net.Listener) context.Context {
-			return serverCtx
-		},
+		Addr:    ":8085",
 		Handler: mux,
 	}
 
+	errChan := make(chan error)
 	go func() {
-		log.Println("Сервер запущен")
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			panic(err)
-		}
+		<-rootCtx.Done()
+		log.Println("Остановка сервара, ожидание завершения текущих запросов")
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		err := server.Shutdown(shutdownCtx)
+		errChan <- err
 	}()
 
-	// ожидание сигнала завершения работы
-	<-rootCtx.Done()
-	// выключаем перехват сигналов
-	stop()
+	log.Println("Сервер запущен")
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Panic(err)
+	}
 
-	log.Println("Остановка сервара, ожидание завершения текущих запросов")
-
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	// отключаем неактивные соединения и ждем завершения запросов
-	err = server.Shutdown(shutdownCtx)
-
-	// если запросы не завершились, отменяем контекст сервера
-	stopServer()
+	// ожидание завершения shutdown
+	err = <-errChan
 	if err != nil {
-		log.Println("Ошибка при остановке сервера, ожидаем еще 3 секунды")
-		time.Sleep(time.Second * 3)
+		log.Println(err)
 	}
 	log.Println("Сервер остановлен")
+
 }
